5befe29f16072cbe353b70393a803659559c3d31
ririndeismillia <ririndeismillia@gmail.com>
2026-06-01 19:47:57 +0700

    kondisi jika file ada

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -43,6 +43,7 @@ int main()
     infile.open(NamaFile + ".txt", ios::in);
 
     cout << endl << ">= Membuka dan membaca file" << endl;
-    
+    //kondisi jika file ada
+    if (infile.is_open())
     
 }
