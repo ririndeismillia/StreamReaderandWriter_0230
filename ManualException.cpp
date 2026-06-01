067c3b463e36df914c173c7f2d94fefb0fd26b6c
ririndeismillia <ririndeismillia@gmail.com>
2026-06-01 19:56:58 +0700

    mambuat isi dari try untuk pengujian error

---
+++ ManualException.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //mambuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integar maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    
+}
