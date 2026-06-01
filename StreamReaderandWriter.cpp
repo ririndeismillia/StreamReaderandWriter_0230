57016cd56002aa8d8b3fc1a11ad912000f9cef93
ririndeismillia <ririndeismillia@gmail.com>
2026-06-01 20:13:49 +0700

    deklarasin variabel baris untuk menyimpan data string

---
+++ StreamReaderandWriter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    //deklarasin variabel baris untuk menyimpan data string 
+    string baris;
+
+    
+
+}
