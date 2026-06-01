a26791dd250061c701577b4cd66ecb29247e12d9
ririndeismillia <ririndeismillia@gmail.com>
2026-06-01 19:24:29 +0700

    deklarasi variable baris dan NamaFile

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{ 
+
+    //deklarasi variable baris dan NamaFile
+    string baris, NamaFile;
+
+    
+}
