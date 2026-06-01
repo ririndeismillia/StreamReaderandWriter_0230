af3b6cef163819c2bcf8841d8fe01ed62ee61a4e
ririndeismillia <ririndeismillia@gmail.com>
2026-06-01 20:18:44 +0700

    menunjuk ke sebuah file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -34,6 +34,10 @@ int main()
 
     //membuka file dalam mode membaca
     ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">=Membuka dan membaca file " << endl;
     
 
 }
